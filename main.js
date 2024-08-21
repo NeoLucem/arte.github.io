c8e4f4a72c1dd18130e6aa4d75586b44f9d6c6de
Deo Gerard Obomby Ondaye <98679519+NeoLucem@users.noreply.github.com>
2024-08-21 12:23:12 +0000

    updated page

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
+const swiper = new Swiper('.swiper', {
+  autoplay: {
+    delay: 2000,
+  },
+  centeredSlides: true,
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 30,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    }
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+
 let closeBtn = document.querySelector('#close-hamburger');
 let openBtn = document.querySelector('#hamburger-btn');
 
